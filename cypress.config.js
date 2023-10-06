74228dd798fe44e86fa4eb409e24d9aa99061cc3
rudnikqa <dmitriirudnik@gmail.com>
2023-10-06 08:55:19 +0200

    retries: 0

--- cypress.config.js
+++ cypress.config.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
     baseUrl: "https://pred-ul.metib.online/",
     supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
     specPattern: 'cypress/e2e/**/**/*.{js,jsx,ts,tsx}',
-    retries: 1,
+    retries: 0,
     video: false,
     defaultCommandTimeout: 60000, //Общий Таймаут
     viewportWidth: 1920,
