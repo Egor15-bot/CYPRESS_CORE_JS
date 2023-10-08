a8c90727601b6a1688a9a196449b4bcdb47860df
Egor Bondar <erop_96_15@mail.ru>
2023-10-08 15:52:14 +1000

    Дописал метод по выбору типа письма

--- cypress/e2e/VED/Снятие с учета.cy.js
+++ cypress/e2e/VED/Снятие с учета.cy.js
@@ -1,23 +1,25 @@
-describe('Снятие с учета Документы',()=>{
-    beforeEach('', () => {
-        cy.loginApi('TOKEN_VED');
-        cy.changeCompanyApi("3894372");
-        cy.visit("/ved");
-      })
+describe("Снятие с учета Документы", () => {
+  beforeEach("", () => {
 
-      it('#1842 - Статусы по снятию с учета',()=>{
-        cy.openVedTab('Документы')
-        cy.openVedSubTab('Прочие документы')
-        cy.setDatePickerTo('Год') 
-        cy.setDocType('Снятие с учета')
+    cy.loginApi('TOKEN_VED');
+    cy.changeCompanyApi("3894372");
+    cy.visit('/ved')
+  });
 
-      })
-      it.only('# - Информация по Снятие с учета',() =>{
-        cy.openVedTab('Документы')
-        cy.openVedSubTab('Прочие документы')
-        cy.setDatePickerTo('Год')
-        cy.get('div.doc-item__item').first().click().then(()=>{
-
-        })
-      })
-    })
+  it("#1842 - Статусы по снятию с учета", () => {
+    cy.openVedTab("Документы");
+    cy.openVedSubTab("Прочие документы");
+    cy.setDatePickerTo("Год");
+    cy.setDocType("Снятие с учета");
+    
+  });
+  it.only("# - Информация по Снятие с учета", () => {
+    cy.openVedTab("Документы");
+    cy.openVedSubTab("Прочие документы");
+    cy.setDatePickerTo("Год");
+    cy.get("div.doc-item__item")
+      .first()
+      .click()
+      .then(() => {});
+  });
+});
