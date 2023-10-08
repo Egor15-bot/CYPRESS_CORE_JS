8336d212b079327016ce1902ea1bccdc024fb79e
Egor Bondar <erop_96_15@mail.ru>
2023-10-08 11:09:23 +1000

    Добавил тесты по снятию с учета

--- cypress/e2e/VED/Снятие с учета.cy.js
+++ cypress/e2e/VED/Снятие с учета.cy.js
@@ -1,12 +1,23 @@
-describe('Снятие с учета',()=>{
+describe('Снятие с учета Документы',()=>{
     beforeEach('', () => {
         cy.loginApi('TOKEN_VED');
         cy.changeCompanyApi("3894372");
         cy.visit("/ved");
       })
 
-      it.only('#1842 - Статусы по снятию с учета',()=>{
+      it('#1842 - Статусы по снятию с учета',()=>{
         cy.openVedTab('Документы')
+        cy.openVedSubTab('Прочие документы')
         cy.setDatePickerTo('Год') 
+        cy.setDocType('Снятие с учета')
+
       })
-})
+      it.only('# - Информация по Снятие с учета',() =>{
+        cy.openVedTab('Документы')
+        cy.openVedSubTab('Прочие документы')
+        cy.setDatePickerTo('Год')
+        cy.get('div.doc-item__item').first().click().then(()=>{
+
+        })
+      })
+    })
--- cypress/support/VED.js
+++ cypress/support/VED.js
@@ -5,3 +5,17 @@ Cypress.Commands.add('setDatePickerTo',(period)=>{
     cy.get('span.title-item.clickable').click()
     cy.get('div.menu-select.open').contains(period).click()
 })
+
+Cypress.Commands.add('openVedSubTab',(tabName)=>{
+    cy.get('div.ved-tabs .tab').contains(tabName).click()
+})
+
+Cypress.Commands.add('setDocType',(docType) =>{
+    cy.get('div.multi-select__title').contains('Все типы').click()
+    cy.get('[data-qa="16589845151673"] div.multi-select__menu > div > app-checkbox').contains('Все типы').uncheck()
+    cy.get('[data-qa="16589845151673"] div.multi-select__menu > div > app-checkbox').contains(docType).check()
+})
+
+Cypress.Commands.add('setDocStatus',(docStatus) =>{
+    cy.get('div.multi-select__title').contains('Все статусы документов').click()
+})
