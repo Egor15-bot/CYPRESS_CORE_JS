1dc77af77f73fca701a170aaba8075bea9247577
rudnikqa <dmitriirudnik@gmail.com>
2023-10-13 13:30:10 +0200

    Обновил cypress.config.js baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
   e2e: {
     viewportWidth: "1920",
     viewportHeight: "1080",
-    baseUrl: "https://pred-ul.metib.online//",
+    baseUrl: "https://pred-ul.metib.online/",
     supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
     specPattern: 'cypress/e2e/**/**/*.{js,jsx,ts,tsx}',
     retries: 0,
